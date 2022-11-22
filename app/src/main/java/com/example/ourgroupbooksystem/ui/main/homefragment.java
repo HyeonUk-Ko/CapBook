5058c0f06b8f95d4e80dca1dfd8b9bb4f41eff47
Hongse0 <hongseyoung981007@gmail.com>
2022-11-22 15:02:51 +0900

    리스트뷰 추가
    리스트뷰를 추가!
    일단 mid라는 배열에 임시 데이터를 넣어서 해놨음 db연동을 해서 작업을 해야할듯.

--- app/src/main/java/com/example/ourgroupbooksystem/ui/main/homefragment.java
+++ app/src/main/java/com/example/ourgroupbooksystem/ui/main/homefragment.java
@@ -2,11 +2,18 @@ package com.example.ourgroupbooksystem.ui.main;
 
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.ourgroupbooksystem.R;
@@ -16,7 +23,28 @@ public class homefragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_homefragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        final String[] mid = {"홍세영","2018101018","캡스톤","안드로이드 스튜디오"};
+        ListView listView= view.findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, mid);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Toast.makeText(getContext(),mid[arg2],Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
