7fdcd0697263e4b7bee7a9bc497d6e6898f7f73d
kohw99@gmail.com <kohw99@gmail.com>
2022-11-28 00:36:15 +0900

    DB값 불러오는거 구현

---
+++ app/src/main/java/com/example/ourgroupbooksystem/ui/main/BookDataVO.java
@@ -0,0 +1,60 @@
+package com.example.ourgroupbooksystem.ui.main;
+
+public class BookDataVO {
+    public String isbn;
+    public String author;
+    public String bookName;
+    public String publishedDate;
+    public String publisher;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public BookDataVO() {
+    }
+
+    public BookDataVO(String isbn, String author, String bookName, String publishedDate, String publisher) {
+        this.isbn = isbn;
+        this.author = author;
+        this.bookName = bookName;
+        this.publishedDate = publishedDate;
+        this.publisher = publisher;
+    }
+}
--- app/src/main/java/com/example/ourgroupbooksystem/ui/main/homefragment.java
+++ app/src/main/java/com/example/ourgroupbooksystem/ui/main/homefragment.java
@@ -12,9 +12,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.ourgroupbooksystem.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class homefragment extends Fragment {
+
+    private DatabaseReference mDatabase;
+    private List<BookDataVO> result = new LinkedList<BookDataVO>();
+    ArrayList<ListData> listViewData = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,39 +41,80 @@ public class homefragment extends Fragment {
         return inflater.inflate(R.layout.fragment_homefragment, container, false);
     }
 
-
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         ListView listView= view.findViewById(R.id.list);
 
-        ArrayList<ListData> listViewData = new ArrayList<>();
-        for (int i = 0; i < 30; ++i) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+//        BookDataVO bookVO = new BookDataVO("1","1","1","1","1");
+//
+//        writeData(bookVO);
+
+//        ValueEventListener postListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // Get Post object and use the values to update the UI
+////                List<BookDataVO> tmpList = new LinkedList<BookDataVO>();
+////                tmpList.add(dataSnapshot.getValue.BookDataVO.class);
+//                BookDataVO post = dataSnapshot.getValue(BookDataVO.class);
+//                // ..
+//                System.err.println("post : "+post);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                // Getting Post failed, log a message
+//                Log.w("loadPost:onCancelled", databaseError.toException());
+//            }
+//        };
+//        mPostReference.addValueEventListener(postListener);
+
+        mDatabase.child("bookList").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                Log.d("firebase", String.valueOf(task.getResult().getValue()));
+
+                for (DataSnapshot ds : task.getResult().getChildren()) {
+                    BookDataVO tmp = ds.getValue(BookDataVO.class);
+                    result.add(tmp);
+                }
+
+                ListAdapter aAdapter = new CustomListView(initListData(result));
+                listView.setAdapter(aAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String clickName = listViewData.get(position).title;
+                        Log.d("확인", "name : " + clickName);
+                    }
+                });
+            }
+        });
+    }
+
+    public void writeData(BookDataVO data) {
+        mDatabase.child("bookList").child("3").setValue(data);
+    }
+
+    public ArrayList<ListData> initListData(List<BookDataVO> result) {
+        System.err.println(result);
+        for (BookDataVO tmp : result) {
             ListData listData = new ListData();
 
             listData.mainImage = R.drawable.ic_launcher_foreground;
             listData.star = R.drawable.ic_launcher_foreground;
 
-            listData.title = " 책" + (i + 1);
-            listData.body_1 = "OO출판사 / OOO";
-            listData.body_2 = "2019.10.13 / 9461754";
+            listData.title = tmp.getBookName();
+            listData.body_1 =  tmp.getPublisher() + " / " + tmp.getAuthor();
+            listData.body_2 = tmp.getPublishedDate() + " / " + tmp.getIsbn();
 
             listViewData.add(listData);
         }
-
-        ListAdapter aAdapter = new CustomListView(listViewData);
-        listView.setAdapter(aAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String clickName = listViewData.get(position).title;
-                Log.d("확인", "name : " + clickName);
-
-            }
-        });
-
+        return listViewData;
     }
 }
