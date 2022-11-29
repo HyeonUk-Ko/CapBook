ef1e44a63810574943a62e62fc2a2418da08fc93
yutufar <37671021+yutufar@users.noreply.github.com>
2022-11-29 11:58:38 +0900

    dialog
    dialog구현

--- app/src/main/java/com/example/ourgroupbooksystem/ui/main/homefragment.java
+++ app/src/main/java/com/example/ourgroupbooksystem/ui/main/homefragment.java
@@ -1,17 +1,17 @@
 package com.example.ourgroupbooksystem.ui.main;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -19,46 +19,39 @@ import com.example.ourgroupbooksystem.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class homefragment extends Fragment {
 
     private DatabaseReference mDatabase;
     private List<BookDataVO> result = new LinkedList<BookDataVO>();
     ArrayList<ListData> listViewData = new ArrayList<>();
+    Dialog dilaog01;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        dilaog01 = new Dialog(getContext());       // Dialog 초기화
+        dilaog01.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dilaog01.setContentView(R.layout.dialog_book);             // xml 레이아웃 파일과 연결
+
+
         return inflater.inflate(R.layout.fragment_homefragment, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ListView listView= view.findViewById(R.id.list);
-        Button searchBtn = (Button) view.findViewById(R.id.searchBtn);
-        EditText searchBox = (EditText) view.findViewById(R.id.searchBox);
-
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(String.valueOf(searchBox)==""){
-                    Toast.makeText(getActivity(), "검색할 데이터를 입력해주세요", Toast.LENGTH_SHORT).show();
-                }
-                searchData(String.valueOf(searchBox));
-            }
-        });
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 //        BookDataVO bookVO = new BookDataVO("1","1","1","1","1");
@@ -92,8 +85,6 @@ public class homefragment extends Fragment {
                 }
                 Log.d("firebase", String.valueOf(task.getResult().getValue()));
 
-                result.clear();
-
                 for (DataSnapshot ds : task.getResult().getChildren()) {
                     BookDataVO tmp = ds.getValue(BookDataVO.class);
                     result.add(tmp);
@@ -105,8 +96,17 @@ public class homefragment extends Fragment {
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String clickName = listViewData.get(position).title;
-                        Log.d("확인", "name : " + clickName);
+                    dilaog01.show();
+
+                    Button close_btn=dilaog01.findViewById(R.id.btn_close);
+                    close_btn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dilaog01.dismiss();
+                        }
+                    });
+
+
                     }
                 });
             }
@@ -118,24 +118,19 @@ public class homefragment extends Fragment {
     }
 
     public ArrayList<ListData> initListData(List<BookDataVO> result) {
-        listViewData.clear();
-            for (BookDataVO tmp : result) {
-                ListData listData = new ListData();
+        System.err.println(result);
+        for (BookDataVO tmp : result) {
+            ListData listData = new ListData();
 
-                listData.mainImage = R.drawable.ic_launcher_foreground;
-                listData.star = R.drawable.ic_launcher_foreground;
+            listData.mainImage = R.drawable.ic_launcher_foreground;
+            listData.star = R.drawable.ic_launcher_foreground;
 
-                listData.title = tmp.getBookName();
-                listData.body_1 =  tmp.getPublisher() + " / " + tmp.getAuthor() + " / " + tmp.getPrice() +"원";
-                listData.body_2 = tmp.getPublishedDate() + " / " + tmp.getIsbn() + " / " + tmp.getQuantitiy() +"권 남음";
-
+            listData.title = tmp.getBookName();
+            listData.body_1 =  tmp.getPublisher() + " / " + tmp.getAuthor();
+            listData.body_2 = tmp.getPublishedDate() + " / " + tmp.getIsbn();
 
             listViewData.add(listData);
         }
         return listViewData;
-    }
-
-    public void searchData (String searchText) {
-
     }
 }
