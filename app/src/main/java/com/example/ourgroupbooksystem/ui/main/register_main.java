1c9f15fdaf3c933f9de461fd8166b1d233636cce
kohw99@gmail.com <kohw99@gmail.com>
2022-11-29 18:07:22 +0900

    로그인 및 회원가입 UI 수정

--- app/src/main/java/com/example/ourgroupbooksystem/ui/main/register_main.java
+++ app/src/main/java/com/example/ourgroupbooksystem/ui/main/register_main.java
@@ -47,7 +47,6 @@ public class register_main extends AppCompatActivity {
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 signUp();
             }
         });
